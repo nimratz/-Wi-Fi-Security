0ed4372d2dfecf7faf48deb4c49070cf7f567a0c
nimratz <44867698+nimratz@users.noreply.github.com>
2018-11-08 14:05:20 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get install aircrack-ng
+git clone https://github.com/aircrack-ng/rtl8812au.git
+sudo apt-get install ocl-icd-libopencl1
+sudo apt-get install nvidia-driver
+sudo apt-get install nvidia-cuda-toolkit
+git clone https://github.com/hashcat/gashcat-utils.git
